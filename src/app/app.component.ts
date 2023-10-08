847067143e6aef303b395184854d3ae78adc2c8f
Agustin <heyius.river@gmail.com>
2023-10-07 22:12:32 -0300

    desafio2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'desafio-2';
+  
+  clientes = ["Leonel","Daniel", "Nicolas"];
+
+  color: number = -1; 
 }
+
+
